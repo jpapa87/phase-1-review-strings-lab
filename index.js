4009c9b5fd634626882b0c66af493484d0a986c1
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-05-22 14:17:58 -0400

    submission

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Buster Douglas'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ${B}!';
+    'Buster Douglas'.charAt(0);
